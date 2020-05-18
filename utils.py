26c07c9c5c36fb49df738470a9ee506609c81933
krieg <none>
2020-05-18 20:17:42 +0800

    add some utility functions

--- utils.py
+++ utils.py
@@ -2,11 +2,80 @@ import librosa
 import numpy as np
 import os
 import pyworld
-
+from pycwt import wavelet
+import pywt
+from sklearn import preprocessing
+from scipy.interpolate import interp1d
+from scipy.signal import firwin, lfilter
 
 def load_wav(wav_file, sr):
     wav, _ = librosa.load(wav_file, sr=sr, mono=True)
     return wav
+
+def convert_continuos_f0(f0):
+    '''CONVERT F0 TO CONTINUOUS F0
+    Args:
+        f0 (ndarray): original f0 sequence with the shape (T)
+    Return:
+        (ndarray): continuous f0 with the shape (T)
+    '''
+    # get uv information as binary
+    uv = np.float32(f0 != 0)
+
+    # get start and end of f0
+    if (f0 == 0).all():
+        logging.warn("all of the f0 values are 0.")
+        return uv, f0
+    start_f0 = f0[f0 != 0][0]
+    end_f0 = f0[f0 != 0][-1]
+
+    # padding start and end of f0 sequence
+    start_idx = np.where(f0 == start_f0)[0][0]
+    end_idx = np.where(f0 == end_f0)[0][-1]
+    f0[:start_idx] = start_f0
+    f0[end_idx:] = end_f0
+
+    # get non-zero frame index
+    nz_frames = np.where(f0 != 0)[0]
+
+    # perform linear interpolation
+    f = interp1d(nz_frames, f0[nz_frames])
+    cont_f0 = f(np.arange(0, f0.shape[0]))
+
+    return uv, cont_f0
+
+def get_lf0_cwt(lf0):
+    '''
+    input: 
+        signal of shape (N)
+    output: 
+        Wavelet_lf0 of shape(10, N), scales of shape(10)
+    '''
+    mother = wavelet.MexicanHat()
+    dt = 0.005
+    dj = 1
+    s0 = dt * 2
+    J = 9
+    
+    Wavelet_lf0, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(np.squeeze(lf0), dt, dj, s0, J, mother)
+    # Wavelet.shape => (J + 1, len(lf0))
+    Wavelet_lf0 = np.real(Wavelet_lf0).T
+    return Wavelet_lf0, scales
+
+def inverse_cwt(Wavelet_lf0, scales):
+    '''
+    Recovering signal
+    input: 
+        Wavelet_lf0 of shape(N, J + 1), scales of shape(J + 1)
+    output:
+        signal of shape(N)
+    '''
+    lf0_rec = np.zeros([Wavelet_lf0.shape[0], len(scales)])
+    for i in range(0, len(scales)):
+        lf0_rec[:,i] = Wavelet_lf0[:,i] * ((i+1+2.5)**(-2.5))
+    lf0_rec_sum = np.sum(lf0_rec, axis = 1)
+    lf0_rec_sum = preprocessing.scale(lf0_rec_sum)
+    return lf0_rec_sum
 
 def world_decompose(wav, fs, frame_period = 5.0):
     # Decompose speech signal into f0, spectral envelope and aperiodicity using WORLD
