a37d941ab756ea4b56586b925301db7556fa3c72
Orange Lee <lycheng1215@sina.com>
2020-05-20 14:55:16 +0800

    支持播放原音频。

--- UI/main.py
+++ UI/main.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 import soundfile as sf
 import sounddevice as sd
 import librosa
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import platform
@@ -22,10 +23,18 @@ class main_window(tk.Tk):
 
         plt.figure(figsize=(4.8, 3.2))
         plt.plot(np.arange(wav.shape[0]), wav)
-        plt.savefig('temp.png')
-        self.image1 = tk.PhotoImage(file='temp.png')  # 需要对图片保持引用
+        plt.title('origin wav')
+        plt.xticks([])
+        plt.savefig('origin.png', dpi=100)
+        self.image1 = tk.PhotoImage(file='origin.png')  # 需要对图片保持引用
         self.canvas1.create_image(0, 0, anchor='nw', image=self.image1)
         return True
+
+    def button1_on_click(self):
+        try:
+            sd.play(self.wav, 16000)
+        except:
+            pass
 
     def init_window(self):
         tk.Tk.__init__(self)
@@ -43,16 +52,35 @@ class main_window(tk.Tk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        # self.grid_rowconfigure(1, weight=1)
 
         self.canvas1 = tk.Canvas(self, width=480, height=320)
         self.canvas1.grid(row=0, column=0)
-        self.canvas2 = tk.Canvas(self)
-        self.canvas2.grid(row=0, column=1, sticky=tk.W+tk.E+tk.N+tk.S)
-        self.canvas2 = tk.Canvas(self)
-        self.canvas2.grid(row=1, column=0, sticky=tk.W+tk.E+tk.N+tk.S)
-        self.canvas2 = tk.Canvas(self)
-        self.canvas2.grid(row=1, column=1, sticky=tk.W+tk.E+tk.N+tk.S)
+        plt.figure(figsize=(4.8, 3.2))
+        plt.title('origin wav')
+        plt.xticks([])
+        plt.yticks([])
+        plt.savefig('origin.png', dpi=100)
+        self.image1 = tk.PhotoImage(file='origin.png')  # 需要对图片保持引用
+        self.canvas1.create_image(0, 0, anchor='nw', image=self.image1)
+
+        self.frame1 = tk.Frame(self)
+        self.frame1.grid(row=0, column=1, sticky=tk.W+tk.E+tk.N+tk.S)
+        self.frame1.grid_columnconfigure(0, weight=1)
+        self.frame1.grid_rowconfigure(0, weight=1)
+
+        self.button1 = tk.Button(
+            self.frame1, text='播放', command=self.button1_on_click)
+        self.button1.grid(row=0, column=0, padx=8, ipadx=8)
+
+        self.list1 = tk.Listbox(self)
+        self.list1.grid(row=1, column=0, sticky=tk.W+tk.E,
+                        padx=8, ipadx=8, pady=8, ipady=8)
+
+        # self.canvas2 = tk.Canvas(self)
+        # self.canvas2.grid(row=1, column=0, sticky=tk.W+tk.E+tk.N+tk.S)
+        # self.canvas2 = tk.Canvas(self)
+        # self.canvas2.grid(row=1, column=1, sticky=tk.W+tk.E+tk.N+tk.S)
 
     def init_instance(self):
         self.init_window()
